369f0c252f454947d55508bfabb763c9a2063d6c
ZehVitor <jvsilvas@hotmail.com>
2016-10-17 15:52:33 -0200

    Script de criação do Banco

---
+++ Banco%20BackUp/Create DB NerdFlix.sql
@@ -0,0 +1,38 @@
+CREATE ROLE admin WITH SUPERUSER LOGIN PASSWORD 'adminlocal';
+CREATE USER admin;
+
+CREATE DATABASE NerdFlixDB OWNER admin;
+
+CREATE TABLE usuario (
+    id_usuario SERIAL NOT NULL PRIMARY KEY,
+    email varchar(255),
+    login varchar(255) UNIQUE NOT NULL,
+    nome varchar(255),
+    senha varchar(255) NOT NULL
+);
+
+CREATE TABLE filme (
+    id_filme SERIAL NOT NULL,
+    duracao integer NOT NULL,
+    genero varchar(255),
+    resolucao varchar(255),
+    sinopse varchar(255),
+    titulo varchar(255)
+);
+
+CREATE TABLE serie (
+    id_filme SERIAL NOT NULL,
+    duracao integer NOT NULL,
+    genero varchar(255),
+    resolucao varchar(255),
+    sinopse varchar(255),
+    episodio integer NOT NULL,
+    temporada integer NOT NULL,
+    titulo varchar(255)
+);
+
+
+INSERT INTO usuario (id_usuario, email, login, nome, senha) VALUES (1, 'default@mail.com', 'admin', 'Administrador', '123');
+INSERT INTO usuario (id_usuario, email, login, nome, senha) VALUES (2, 'Outro@mail.com', 'ze', 'JVior', '123');
+INSERT INTO usuario (id_usuario, email, login, nome, senha) VALUES (3, 'Outro@mail.com', 'ed', 'Edye', '321');
+INSERT INTO usuario (id_usuario, email, login, nome, senha) VALUES (4, 'outro@mail.com', 'tay', 'Tayanne', '1234');
